f07284edb1dfa39bb5c4e3bff938af31135717cf
Steve Narancic <steve.mello@narancic.com.br>
2021-10-27 15:49:09 -0300

    .

--- index.php
+++ index.php
@@ -1,24 +1,14 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
 
-$pagina = 'home';
-if (isset($_GET['pagina'])) {
-    $pagina = $_GET['pagina'];
-}
-
 $loader = new Twig\Loader\FilesystemLoader(__DIR__ . '/app/view');
 $twig = new Twig\Environment($loader, [
     'cache' => __DIR__ . '/app/view/cache',
     'cache' => false,
 ]);
-$twig->addGlobal('pagina_atual', $pagina);
 
-switch ($pagina) {
-    case 'home':
-        echo $twig->render('home.twig');
-        break;
-    default:
-        header('HTTP/1.0 404 Not Found');
-        echo $twig->render('404.twig');
-        break;
+if ($_GET) {
+    $url = explode('/', $_GET['url']);
+
+    echo $twig->render($url[0] . '.twig');
 }

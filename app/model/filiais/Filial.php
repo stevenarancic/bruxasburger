842dc3aaf213d49be0b3d0c7ec9f501a512c07aa
Steve Narancic <steve.mello@narancic.com.br>
2021-10-28 11:13:04 -0300

    .
    criação da classe Filial e do arquivo FilialDAO

---
+++ app/model/filiais/Filial.php
@@ -0,0 +1,154 @@
+<?php
+
+namespace app\model\filiais;
+
+class Filial
+{
+    private $id;
+    private $telefone;
+    private $uf;
+    private $cidade;
+    private $bairro;
+    private $rua;
+    private $numero;
+
+    /**
+     * Get the value of id
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set the value of id
+     *
+     * @return  self
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of telefone
+     */
+    public function getTelefone()
+    {
+        return $this->telefone;
+    }
+
+    /**
+     * Set the value of telefone
+     *
+     * @return  self
+     */
+    public function setTelefone($telefone)
+    {
+        $this->telefone = $telefone;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of uf
+     */
+    public function getUf()
+    {
+        return $this->uf;
+    }
+
+    /**
+     * Set the value of uf
+     *
+     * @return  self
+     */
+    public function setUf($uf)
+    {
+        $this->uf = $uf;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of cidade
+     */
+    public function getCidade()
+    {
+        return $this->cidade;
+    }
+
+    /**
+     * Set the value of cidade
+     *
+     * @return  self
+     */
+    public function setCidade($cidade)
+    {
+        $this->cidade = $cidade;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of bairro
+     */
+    public function getBairro()
+    {
+        return $this->bairro;
+    }
+
+    /**
+     * Set the value of bairro
+     *
+     * @return  self
+     */
+    public function setBairro($bairro)
+    {
+        $this->bairro = $bairro;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of rua
+     */
+    public function getRua()
+    {
+        return $this->rua;
+    }
+
+    /**
+     * Set the value of rua
+     *
+     * @return  self
+     */
+    public function setRua($rua)
+    {
+        $this->rua = $rua;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of numero
+     */
+    public function getNumero()
+    {
+        return $this->numero;
+    }
+
+    /**
+     * Set the value of numero
+     *
+     * @return  self
+     */
+    public function setNumero($numero)
+    {
+        $this->numero = $numero;
+
+        return $this;
+    }
+}

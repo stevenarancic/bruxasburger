794b9b5c3106bf53bb04511637b698ec7cfdfef8
Steve Narancic <steve.mello@narancic.com.br>
2021-10-28 11:50:27 -0300

    .
    criados os métodos de CRUD na classe FilialDAO.

---
+++ app/model/filiais/FilialDAO.php
@@ -0,0 +1,64 @@
+<?php
+
+use App\Model\Conexao;
+use app\model\filiais\Filial;
+
+class FilialDAO
+{
+    public function createFilial(Filial $filial)
+    {
+        $sql = "INSERT INTO filial(telefone, uf, cidade, bairro, rua, numero) VALUES(:telefone, :uf, :cidade, :bairro, :rua, :numero)";
+
+        $stmt = Conexao::getInstance()->prepare($sql);
+
+        $stmt->bindValue(':telefone', $filial->getTelefone());
+        $stmt->bindValue(':uf', $filial->getUf());
+        $stmt->bindValue(':cidade', $filial->getCidade());
+        $stmt->bindValue(':bairro', $filial->getBairro());
+        $stmt->bindValue(':rua', $filial->getRua());
+        $stmt->bindValue(':numero', $filial->getNumero());
+
+        $stmt->execute();
+    }
+
+    public function readFilial()
+    {
+        $sql = "SELECT * FROM filial";
+
+        $stmt = Conexao::getInstance()->prepare($sql);
+        $stmt->execute();
+
+        if ($stmt->rowCount() > 0) {
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        } else {
+            echo "Nenhuma filial foi cadastrada :(";
+        }
+    }
+
+    public function updateFilial(Filial $filial)
+    {
+        $sql = "UPDATE filial SET telefone = :telefone, uf = :uf, cidade = :cidade, bairro = :bairro, rua = :rua, numero = :numero WHERE id = :id";
+
+        $stmt = Conexao::getInstance()->prepare($sql);
+
+        $stmt->bindValue(':telefone', $filial->getTelefone());
+        $stmt->bindValue(':uf', $filial->getUf());
+        $stmt->bindValue(':cidade', $filial->getCidade());
+        $stmt->bindValue(':bairro', $filial->getBairro());
+        $stmt->bindValue(':rua', $filial->getRua());
+        $stmt->bindValue(':numero', $filial->getNumero());
+
+        $stmt->execute();
+    }
+
+    public function deleteFilial($id)
+    {
+        $sql = "DELETE FROM filial WHERE id = :id";
+
+        $stmt = Conexao::getInstance()->prepare($sql);
+
+        $stmt->bindValue(':id', $id);
+
+        $stmt->execute();
+    }
+}

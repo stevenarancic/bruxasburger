9264c6a21f40a3bf4bf6a3c9d0ee8d4774159f59
Steve Narancic <steve.mello@narancic.com.br>
2021-10-31 17:31:46 -0300

    Update login.php
    login funcionando e redirecionando para o home do gerenciamento.

--- app/controller/usuarios/login.php
+++ app/controller/usuarios/login.php
@@ -1,14 +1,19 @@
 <?php
+require_once '../../../vendor/autoload.php';
 
 use app\model\Conexao;
-
-echo "{$_POST['login']} senha {$_POST['senha']}";
 
 $sql = "SELECT * FROM usuario WHERE login = :login AND senha = :senha";
 
 $stmt = Conexao::getInstance()->prepare($sql);
 
-$stmt->bindValue(':login', );
-$stmt->bindValue(':senha');
+$stmt->bindValue(':login', $_POST['login']);
+$stmt->bindValue(':senha', $_POST['senha']);
 
 $stmt->execute();
+
+if ($stmt->rowCount() > 0) {
+    header("location: ../../../gerenciamento/home");
+} else {
+    echo "Login incorreto!";
+}

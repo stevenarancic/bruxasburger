12287aec26249c49cb2eede35257fdcef842fc9b
Steve Narancic <steve.mello@narancic.com.br>
2021-11-01 16:17:03 -0300

    .

--- index.php
+++ index.php
@@ -12,18 +12,24 @@ $twig = new Twig\Environment($loader, [
     'cache' => false,
 ]);
 
+// Filial
 if (isset($_GET['id']) and $_GET['id'] != "") {
     foreach ($filialDAO->filtrarFilial($_GET['id']) as $key => $filial) {
         echo $twig->render("gerenciamento/filiais/update.html", ['filial' => $filial]);
     }
 }
-
 if (isset($_GET['id_delete']) and $_GET['id_delete'] != "") {
     header("location: app/controller/filiais/delete.php?id_delete={$_GET['id_delete']}");
 }
 
+// Categoria
 if (isset($_GET['id_delete_categoria']) and $_GET['id_delete_categoria'] != "") {
     header("location: app/controller/categorias/delete.php?id_delete_categoria={$_GET['id_delete_categoria']}");
+}
+
+// Item Cardápio
+if (isset($_GET['id_delete_itemcardapio']) and $_GET['id_delete_itemcardapio'] != "") {
+    header("location: app/controller/itens_cardapio/delete.php?id_delete_itemcardapio={$_GET['id_delete_itemcardapio']}");
 }
 
 if ($_GET) {

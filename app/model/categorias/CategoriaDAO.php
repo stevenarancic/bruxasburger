29c819f5991a1260b648a6390f5d291f0d70c26b
Steve Narancic <steve.mello@narancic.com.br>
2021-11-01 08:58:06 -0300

    .
    criada estrutura do modelo e classe de categorias.

---
+++ app/model/categorias/CategoriaDAO.php
@@ -0,0 +1,25 @@
+<?php
+namespace app\model\categorias;
+
+class CategoriaDAO
+{
+    public function create(Categoria $categoria)
+    {
+        # code...
+    }
+
+    public function read()
+    {
+        # code...
+    }
+
+    public function update(Categoria $categoria)
+    {
+        # code...
+    }
+
+    public function delete($id)
+    {
+        # code...
+    }
+}

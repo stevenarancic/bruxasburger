cc267682c9ce6f3db20d10615909c67a2e86018f
Steve Narancic <steve.mello@narancic.com.br>
2021-11-04 15:37:35 -0300

    .
    corrigido bug em que se nenhuma imagem fosse enviada ao editar o item do cardápio, a imagem anterior era apagada.

--- app/controller/itens_cardapio/update.php
+++ app/controller/itens_cardapio/update.php
@@ -3,13 +3,20 @@ require_once __DIR__ . "../../../../vendor/autoload.php";
 
 $itemCardapioDAO = new \app\model\itens_cardapio\ItemCardapioDAO();
 
-$itemCardapio = new \app\model\itens_cardapio\ItemCardapio($_POST['nome'], $_POST['descricao'], $_POST['select_categoria'], $_FILES['imagem_item_cardapio']['name']);
-$itemCardapio->setId($_POST['id']);
-
-$nomeImagem = $_FILES['imagem_item_cardapio']['name'];
-
-$itemCardapioDAO->updateImagemItemCardapio($nomeImagem);
-
-$itemCardapioDAO->updateItemCardapio($itemCardapio);
+if ($_POST['imagem'] == "" and $_FILES['imagem_item_cardapio']['name'] != "") {
+    $itemCardapio = new \app\model\itens_cardapio\ItemCardapio($_POST['nome'], $_POST['descricao'], $_POST['select_categoria'], $_FILES['imagem_item_cardapio']['name']);
+    $itemCardapio->setId($_POST['id']);
+    $itemCardapioDAO->updateImagemItemCardapio($_FILES['imagem_item_cardapio']['name']);
+    $itemCardapioDAO->updateItemCardapio($itemCardapio);
+} else if ($_POST['imagem'] != "" and $_FILES['imagem_item_cardapio']['name'] == "") {
+    $itemCardapio = new \app\model\itens_cardapio\ItemCardapio($_POST['nome'], $_POST['descricao'], $_POST['select_categoria'], $_POST['imagem']);
+    $itemCardapio->setId($_POST['id']);
+    $itemCardapioDAO->updateItemCardapio($itemCardapio);
+} else if ($_POST['imagem'] != "" and $_FILES['imagem_item_cardapio']['name'] != "") {
+    $itemCardapio = new \app\model\itens_cardapio\ItemCardapio($_POST['nome'], $_POST['descricao'], $_POST['select_categoria'], $_FILES['imagem_item_cardapio']['name']);
+    $itemCardapio->setId($_POST['id']);
+    $itemCardapioDAO->updateImagemItemCardapio($_FILES['imagem_item_cardapio']['name']);
+    $itemCardapioDAO->updateItemCardapio($itemCardapio);
+}
 
 header("location: ../../../gerenciamento/cardapio/itens/home");

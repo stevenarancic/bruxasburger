881df0acdd28c3e6b07a3d6d6072f9f67b973896
Steve Narancic <steve.mello@narancic.com.br>
2021-11-04 15:56:14 -0300

    Update update.php

--- app/controller/itens_cardapio/update.php
+++ app/controller/itens_cardapio/update.php
@@ -6,17 +6,14 @@ $itemCardapioDAO = new \app\model\itens_cardapio\ItemCardapioDAO();
 $imagemBanco = $_POST['imagem'];
 $imagemUpload = $_FILES['imagem_item_cardapio']['name'];
 
-if ($imagemUpload == "") {
-    $itemCardapio = new \app\model\itens_cardapio\ItemCardapio($_POST['nome'], $_POST['descricao'], $_POST['select_categoria'], $imagemBanco);
-    $itemCardapio->setId($_POST['id']);
-    $itemCardapioDAO->updateItemCardapio($itemCardapio);
-}
-
 if ($imagemUpload != "") {
     $itemCardapio = new \app\model\itens_cardapio\ItemCardapio($_POST['nome'], $_POST['descricao'], $_POST['select_categoria'], $imagemUpload);
-    $itemCardapio->setId($_POST['id']);
     $itemCardapioDAO->updateImagemItemCardapio($imagemUpload);
-    $itemCardapioDAO->updateItemCardapio($itemCardapio);
+} else {
+    $itemCardapio = new \app\model\itens_cardapio\ItemCardapio($_POST['nome'], $_POST['descricao'], $_POST['select_categoria'], $imagemBanco);
 }
+
+$itemCardapio->setId($_POST['id']);
+$itemCardapioDAO->updateItemCardapio($itemCardapio);
 
 header("location: ../../../gerenciamento/cardapio/itens/home");

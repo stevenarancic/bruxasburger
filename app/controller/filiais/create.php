d5a592208a017316480b3591bbe96bb7f504fc9e
Steve Narancic <steve.mello@narancic.com.br>
2021-11-08 01:12:41 -0300

    .
    criado upload de múltiplas imagens no formulário de cadastro de filial.

--- app/controller/filiais/create.php
+++ app/controller/filiais/create.php
@@ -4,6 +4,33 @@ require_once __DIR__ . "../../../../vendor/autoload.php";
 $filial = new \app\model\filiais\Filial($_POST['telefone'], $_POST['uf'], $_POST['cidade'], $_POST['bairro'], $_POST['rua'], $_POST['numero']);
 $filialDAO = new \app\model\filiais\FilialDAO();
 
+//$files = array_filter($_FILES['upload']['name']); //something like that to be used before processing files.
+
+// Count # of uploaded files in array
+$total = count($_FILES['upload']['name']);
+
+// Loop through each file
+for ($i = 0; $i < $total; $i++) {
+
+    //Get the temp file path
+    $tmpFilePath = $_FILES['upload']['tmp_name'][$i];
+
+    //Make sure we have a file path
+    if ($tmpFilePath != "") {
+        //Setup our new file path
+        $newFilePath = "../../../assets/img/filiais/" . $_FILES['upload']['name'][$i];
+
+        //Upload the file into the temp dir
+        if (move_uploaded_file($tmpFilePath, $newFilePath)) {
+
+            //Handle other code here
+
+        }
+    }
+}
+
+die();
+
 $filialDAO->createFilial($filial);
 
 header("location: ../../../gerenciamento/filiais/home");

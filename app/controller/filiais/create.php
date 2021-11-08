3ad0553ee21cff824079a2ec223754079994631b
Steve Narancic <steve.mello@narancic.com.br>
2021-11-08 10:50:22 -0300

    .
    cadastro de múltiplas imagens 100% funcional (o upload esta funcionando e as imagens estão sendo cadastradas no banco de dados com o id da filial como FK)

--- app/controller/filiais/create.php
+++ app/controller/filiais/create.php
@@ -1,37 +1,38 @@
 <?php
+
+use app\model\Conexao;
+
 require_once __DIR__ . "../../../../vendor/autoload.php";
 
 $filial = new \app\model\filiais\Filial($_POST['telefone'], $_POST['uf'], $_POST['cidade'], $_POST['bairro'], $_POST['rua'], $_POST['numero']);
 $filialDAO = new \app\model\filiais\FilialDAO();
 
-//$files = array_filter($_FILES['upload']['name']); //something like that to be used before processing files.
+$filialDAO->createFilial($filial);
+setcookie('ultimoIdFilial', Conexao::getInstance()->lastInsertId(), time() + (86400 * 30), "/"); // 86400 = 1 dia. Cookie que dura 1 mês.
 
-// Count # of uploaded files in array
 $total = count($_FILES['upload']['name']);
 
 if (!file_exists("../../../assets/img/filiais/{$_POST['cidade']}")) {
     mkdir("../../../assets/img/filiais/{$_POST['cidade']}", 0777, true);
 }
 
-// Loop through each file
 for ($i = 0; $i < $total; $i++) {
-
-    //Get the temp file path
     $tmpFilePath = $_FILES['upload']['tmp_name'][$i];
 
-    //Make sure we have a file path
     if ($tmpFilePath != "") {
-
-        //Setup our new file path
         $newFilePath = "../../../assets/img/filiais/{$_POST['cidade']}/" . $_FILES['upload']['name'][$i];
 
-        //Upload the file into the temp dir
-        if (move_uploaded_file($tmpFilePath, $newFilePath)) {
-
-            //Handle other code here
-
-        }
+        move_uploaded_file($tmpFilePath, $newFilePath);
     }
+
+    if ($_POST['id'] == "") {
+        $imagemFilial = new \app\model\filiais\ImagemFilial($_COOKIE['ultimoIdFilial'], $_FILES['upload']['name']);
+    } else {
+        $imagemFilial = new \app\model\filiais\ImagemFilial($_POST['id'], $_FILES['upload']['name']);
+    }
+
+    $imagemFilialDAO = new \app\model\filiais\ImagemFilialDAO();
+    $imagemFilialDAO->createImagemFilial($imagemFilial);
 }
 
 // Cadastrar cada imagem do loop na tabela imagem_filial e colocar o id da filial na FK
@@ -40,7 +41,5 @@ for ($i = 0; $i < $total; $i++) {
 //   setcookie('ultimoIdFilial', Conexao::getInstance()->lastInsertId(), time() + (86400 * 30), "/"); // 86400 = 1 dia. Cookie que dura 1 mês.
 //   deletar o registro vazio criado na tabela.
 // }
-
-$filialDAO->createFilial($filial);
 
 header("location: ../../../gerenciamento/filiais/home");
--- app/model/filiais/ImagemFilialDAO.php
+++ app/model/filiais/ImagemFilialDAO.php
@@ -2,14 +2,26 @@
 
 namespace app\model\filiais;
 
+use app\model\Conexao;
+
 class ImagemFilialDAO
 {
-    public function createImagemFilial($imagem_filial)
+    public function createImagemFilial(ImagemFilial $imagem_filial)
     {
+        $sql = "INSERT INTO imagem_filial(nome, filial_id) VALUES(:nome, :filial_id)";
 
+        $stmt = Conexao::getInstance()->prepare($sql);
+
+        $stmt->bindValue(':nome', $imagem_filial->getNome());
+        $stmt->bindValue(':filial_id', $imagem_filial->getFilialId());
+
+        $stmt->execute();
     }
+
     public function deleteImagemFilial($id)
     {
-
+        $sql = "DELETE FROM imagem_filial WHERE id = {$id}";
+        $stmt = Conexao::getInstance()->prepare($sql);
+        $stmt->execute();
     }
 }

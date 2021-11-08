6aab52c249bdd83dd0c1b81631dc1f98578295a7
Steve Narancic <steve.mello@narancic.com.br>
2021-11-08 02:44:40 -0300

    .
    criada classe ImagemFilial (completa) e classe ImagemFilialDAO (sem nenhum método.)

---
+++ app/model/filiais/ImagemFilialDAO.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace app\model\filiais;
+
+class ImagemFilialDAO
+{
+    public function createImagemFilial()
+    {
+
+    }
+    public function readImagemFilial()
+    {
+
+    }
+    public function updateImagemFilial()
+    {
+
+    }
+    public function deleteImagemFilial()
+    {
+
+    }
+}

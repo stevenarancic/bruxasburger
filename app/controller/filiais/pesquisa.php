3d34cd6bbcc751078c1eece87f66d899a16b181e
Steve Narancic <steve.mello@narancic.com.br>
2021-11-11 14:45:55 -0300

    Update pesquisa.php
    agora a pesquisa de filiais tem as imagens das filiais (antes era uma imagem fixa)

--- app/controller/filiais/pesquisa.php
+++ app/controller/filiais/pesquisa.php
@@ -8,15 +8,20 @@ $sql = "SELECT * FROM filial WHERE cidade LIKE '%" . $_POST['cidade'] . "%'";
 $stmt = Conexao::getInstance()->prepare($sql);
 $stmt->execute();
 
+$imagemFilialDAO = new \app\model\filiais\ImagemFilialDAO();
+
 if ($stmt->rowCount() > 0) {
     foreach ($stmt->fetchAll(PDO::FETCH_ASSOC) as $key => $item) {
         echo "
         <div class=\"card border-light mb-3 shadow-lg\" style=\"position: inherit\">
             <div class=\"row g-0\">
-                <div class=\"col-md-4\" style=\"display: grid; place-items:center;\">
-                    <img src=\"https://img.freepik.com/vetores-gratis/imagens-animadas-abstratas-neon-lines_23-2148344065.jpg?size=626&ext=jpg\"
-                        class=\"img-fluid rounded\" alt=\"Imagem indisponível :(\">
-                </div>
+                <div class=\"col-md-4\" style=\"display: grid; place-items:center;\">"
+        ;
+        foreach ($imagemFilialDAO->filtrarPorFilial($item['id']) as $key => $itemImagem) {
+            echo "<img src=\"assets/img/filiais/{$item['cidade']}/{$itemImagem['nome']}\"
+            class=\"img-fluid rounded\" alt=\"Imagem indisponível :(\" style=\"height: 10rem; width: 100%; object-fit: cover\">";
+        }
+        echo "</div>
                 <div class=\"col-md-8\">
                     <div class=\"card-body\">
                         <h5 class=\"card-title\">Bruxas Burger {$item['cidade']}</h5>

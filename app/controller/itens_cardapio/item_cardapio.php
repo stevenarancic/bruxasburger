1c96c2ce9adfaf00d065777c0b5ec88c02a83478
Steve Narancic <steve.mello@narancic.com.br>
2021-12-08 15:23:15 -0300

    .

--- app/controller/itens_cardapio/item_cardapio.php
+++ app/controller/itens_cardapio/item_cardapio.php
@@ -4,11 +4,12 @@
             <img src="assets/img/cardapio_itens/<?=$item['imagem']?>" class="img-fluid rounded-start" alt="...">
         </div>
         <div class="col-md-8">
-            <div class="card-header bg-danger text-white" style="font-weight: bold;">
+            <div class="card-header"
+                style="font-weight: bold; border-top-left-radius: 0; background-color: dimgrey; color: white">
                 <?=$item['nome']?>&#<?=$item['icone']?>;
             </div>
             <div class="card-body">
-                <h4 class="card-title"><?=$item['item_nome']?></h4>
+                <h4 class="card-title fs-3"><?=$item['item_nome']?></h4>
                 <p class="card-text"><?=$item['descricao']?></p>
             </div>
         </div>
